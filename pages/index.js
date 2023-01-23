ee6494405aaff20d3248559162a1b59fce9250e0
sunilbudchauhan <123384669+sunilbudchauhan@users.noreply.github.com>
2023-01-23 19:50:44 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Stock research team</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="UNDER CONSTRUCTION" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          New site loading soon!
         </p>
       </main>
 
